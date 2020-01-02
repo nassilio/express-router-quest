48c4d6dcaf7f09eca849ec1e9192be115ffe4c77
nassilio <hubmattfac@fac.com>
2020-01-02 14:35:39 +0100

    quest done

---
+++ routes/tags.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const fakeTags = require("../data/tags");
+const fakePosts = require("../data/posts");
+const router = express.Router({ mergeParams: true });
+
+router.get("/", (req, res) => {
+  res.json(fakeTags);
+});
+
+router.get("/:tagId/posts", (req, res) => {
+  const tagId = Number(req.params.tagId);
+  const foundPostById = fakePosts.filter(tag => tag.tag_ids === tagId);
+  if (!foundPostById) {
+    return res.status(404).json({
+      error: "Post not found"
+    });
+  }
+  return res.json(foundPostById);
+});
+
+module.exports = router;
